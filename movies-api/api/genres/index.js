42ea9638454968ebd1beb869f655ef3d8712a69f
LuoYang <luoyangyuli2001@gmail.com>
2022-12-05 14:02:16 +0000

    Mongo lab Exercises done

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { genres } from './genresData';
+import Genre from './genreModel';
 
 const router = express.Router(); 
 
-// Get movie details
-router.get('/', (req, res) => {
-  res.status(200).json(genres)
+// Get all users
+router.get('/', async (req, res) => {
+  const genres = await Genre.find();
+  res.status(200).json(genres);
 });
 
 export default router; 
